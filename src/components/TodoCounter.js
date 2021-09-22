b80eb3546d27c1170b1dfd111f7029623bd54931
edinsonrequena <edinsonrequena@protonmail.com>
2021-09-22 16:59:14 -0400

    Basic Layout css

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from '../assets/styles/components/TodoCounter.css'
 
 function TodoCounter() {
     return (
--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from '../assets/styles/components/TodoSearch.css'
 
 function TodoSearch() {
     return (
